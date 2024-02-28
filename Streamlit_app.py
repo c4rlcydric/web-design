14adc9caa236c90a2b647b71094991ec7b3259b6
c4rlcydric <cydricone@gmail.com>
2024-02-28 21:58:29 +0800

    secondary-web-design

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,4 +1,4 @@
-from pathlib import Path 
+from pathlib import Path
 import json
 import streamlit as st
 from streamlit_lottie import st_lottie
@@ -11,44 +11,42 @@ ASSETS = THIS_DIR / "assets"
 LOTTIE_ANIMATION = ASSETS / "Polite_Chicky.json"
 
 
-# Function to Load and display the Lottie animation
+# Function to load and display the Lottie animation
 def load_lottie_animation(file_path):
     with open(file_path, "r") as f:
         return json.load(f)
-    
+
 
 # Function to apply snowfall effect
 def run_snow_animation():
-    rain(emoji="🥳", font_size=20, falling_speed=5, animation_length="infinite")
+    rain(emoji="❄️", font_size=20, falling_speed=5, animation_length="infinite")
 
 
 # Function to get the name from query parameters
 def get_person_name():
     query_params = st.experimental_get_query_params()
-    return query_params.get("name", ["Yeisha"])[0]
+    return query_params.get("name", ["YEISHA"])[0]
 
 
-#page configuration
-st.set_page_config(page_title= "happy birthday", page_icon= "🥰")
+# Page configuration
+st.set_page_config(page_title="Happy Birthday", page_icon="🎄")
 
-# run snowfall animation
+# Run snowfall animation
 run_snow_animation()
 
-#apply custom css
+# Apply custom CSS
 with open(CSS_FILE) as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-#display header with personalized name
+# Display header with personalized name
 PERSON_NAME = get_person_name()
-st.header(f"happy birthday, {PERSON_NAME}!🥳"  ,anchor=False)
+st.header(f"Happy Holidays, {PERSON_NAME}! 🎄", anchor=False)
 
-#display the lottie animation
+# Display the Lottie animation
 lottie_animation = load_lottie_animation(LOTTIE_ANIMATION)
-st_lottie(lottie_animation, key="Polite_Chicky", height=300)
+st_lottie(lottie_animation, key="lottie-holiday", height=300)
 
-#pesonalized holiday message
+# Personalized holiday message
 st.markdown(
-    f"Dear {PERSON_NAME}, wishing you happy birthday and more birthday to come sha. 🥳"
+    f"Dear {PERSON_NAME}, wishing you a wonderful holiday season filled with joy and peace. 🌟"
 )
-    
-
